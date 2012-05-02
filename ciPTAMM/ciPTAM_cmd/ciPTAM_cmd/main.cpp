c256c73c63e8482c9bd15d80b93fca21447e5cc8
Patrick Hebron <patchiehebron@gmail.com>
2012-05-02 18:05:47 -0400

    minor

---
+++ ciPTAMM/ciPTAM_cmd/ciPTAM_cmd/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  ciPTAM_cmd
+//
+//  Created by Patrick Hebron on 3/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
